4c3a74ff7e12b86b2622e62334107675430a31d1
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-23 15:57:24 +0530

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Main />
     </div>
   );
 }
-
 export default App;
