1009c4556836fab839c6faeb4a5288402dd85b7e
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-24 15:00:10 +0530

    debouncing added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,46 @@
+import { useDispatch } from "react-redux";
 import * as S from "./Navbar.styled";
+import { addVideos } from "../../store/VideoListSlice";
 
 function Navbar() {
+  let setTimeoutId: any = null;
+  const dispatch = useDispatch();
+
+  function search(key: string) {
+    if (setTimeoutId !== null) {
+      clearTimeout(setTimeoutId);
+    }
+
+    setTimeoutId = setTimeout(() => {
+      fetch(
+        `https://www.googleapis.com/youtube/v3/search?type=videos&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet&q=${key}&maxResults=20`
+      )
+        .then((response: any) => {
+          return response.json();
+        })
+        .then((json) => {
+          const items = json.items;
+          const ids = items.map((item: any) => {
+            return item.id.videoId;
+          });
+          console.log(ids.join(","));
+          fetch(
+            `${
+              process.env.REACT_APP_YOUTUBE_VIDEO_LIST
+            }part=player,statistics,snippet&key=${
+              process.env.REACT_APP_YOUTUBE_API_KEY
+            }&id=${ids.join(",")}&maxResults=20`
+          )
+            .then((response) => {
+              return response.json();
+            })
+            .then((json) => dispatch(addVideos(items)));
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }, 800);
+  }
   return (
     <S.Container>
       <S.Menu>
@@ -12,7 +52,11 @@ function Navbar() {
         </a>
       </S.Menu>
       <S.Search>
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => search(e.target.value)}
+        />
         <S.SearchIcon>
           <img src="/images/search.svg" alt="search-icon" />
         </S.SearchIcon>
