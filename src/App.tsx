da015d55f6aee1e684ff955d3184769285c38e84
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-29 13:54:29 +0530

    added persist sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
+import { auth } from "./firebase";
+import { setUser } from "./store/UserSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      dispatch(setUser(user));
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
