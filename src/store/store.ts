fa55226d5956117ac1c1a151f2fd1dc9dc1bd30d
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-29 13:51:50 +0530

    change in store config

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import UserSlice from "./UserSlice";
 import VideoListSlice from "./VideoListSlice";
 
 const store = configureStore({
   reducer: {
     videos: VideoListSlice,
+    user: UserSlice,
   },
+  middleware: () =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
